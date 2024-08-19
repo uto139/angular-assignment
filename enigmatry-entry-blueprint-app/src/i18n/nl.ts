3995990df9d43524843459a66be814bd61f886b0
Uros Totovic <uto@enigmatry.com>
2024-08-19 15:53:42 +0200

    app renamed

--- enigmatry-entry-blueprint-app/src/i18n/nl.ts
+++
@@ -1,6 +0,0 @@
-export const localeId = 'nl-NL';
-
-export { default as locale } from '@angular/common/locales/nl';
-export { default as localeExtra } from '@angular/common/locales/extra/nl';
-
-export { default as messages } from './messages.nl.json';
