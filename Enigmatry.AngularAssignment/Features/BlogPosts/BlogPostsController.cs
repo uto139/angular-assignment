3973d036ce3e011e98716a0282c9fa3ebb0e3028
Uros Totovic <uto@enigmatry.com>
2024-10-31 12:53:52 +0100

    file scoped namespace

--- Enigmatry.AngularAssignment/Features/BlogPosts/BlogPostsController.cs
+++ Enigmatry.AngularAssignment/Features/BlogPosts/BlogPostsController.cs
@@ -3,56 +3,55 @@ using Enigmatry.AngularAssignment.Api.Services.BlogPosts;
 using Microsoft.AspNetCore.Mvc;
 using System.Net.Mime;
 
-namespace Enigmatry.AngularAssignment.Api.Features.BlogPosts
+namespace Enigmatry.AngularAssignment.Api.Features.BlogPosts;
+
+[Produces(MediaTypeNames.Application.Json)]
+[Route("api/[controller]")]
+public class BlogPostsController : ControllerBase
 {
-    [Produces(MediaTypeNames.Application.Json)]
-    [Route("api/[controller]")]
-    public class BlogPostsController : ControllerBase
+    private readonly ILogger<BlogPostsController> _logger;
+    private readonly BlogPostService _blogService;
+
+    public BlogPostsController(ILogger<BlogPostsController> logger, BlogPostService blogService)
     {
-        private readonly ILogger<BlogPostsController> _logger;
-        private readonly BlogPostService _blogService;
+        _logger = logger;
+        _blogService = blogService;
+    }
 
-        public BlogPostsController(ILogger<BlogPostsController> logger, BlogPostService blogService)
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public async Task<ActionResult<List<GetBlogPosts.Response>>> Search([FromQuery] string? keyword, [FromQuery] BlogPostCategory? category)
+    {
+        var blogPosts = await _blogService.Search(keyword, category);
+        return Ok(blogPosts);
+    }
+
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<ActionResult> CreateOrUpdate([FromBody] BlogPost blogPost)
+    {
+        if (blogPost == null)
         {
-            _logger = logger;
-            _blogService = blogService;
+            return BadRequest();
         }
 
-        [HttpGet]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        public async Task<ActionResult<List<GetBlogPosts.Response>>> Search([FromQuery] string? keyword, [FromQuery] BlogPostCategory? category)
+        await _blogService.CreateOrUpdate(blogPost);
+        return Ok();
+    }
+
+    [HttpDelete("{id:guid}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<ActionResult> Delete(Guid id)
+    {
+        var blogPost = await _blogService.GetAll();
+        if (blogPost.All(x => x.Id != id))
         {
-            var blogPosts = await _blogService.Search(keyword, category);
-            return Ok(blogPosts);
+            return NotFound();
         }
 
-        [HttpPost]
-        [ProducesResponseType(StatusCodes.Status201Created)]
-        [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public async Task<ActionResult> CreateOrUpdate([FromBody] BlogPost blogPost)
-        {
-            if (blogPost == null)
-            {
-                return BadRequest();
-            }
-
-            await _blogService.CreateOrUpdate(blogPost);
-            return Ok();
-        }
-
-        [HttpDelete("{id:guid}")]
-        [ProducesResponseType(StatusCodes.Status204NoContent)]
-        [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public async Task<ActionResult> Delete(Guid id)
-        {
-            var blogPost = await _blogService.GetAll();
-            if (blogPost.All(x => x.Id != id))
-            {
-                return NotFound();
-            }
-
-            await _blogService.Delete(id);
-            return NoContent();
-        }
+        await _blogService.Delete(id);
+        return NoContent();
     }
 }
