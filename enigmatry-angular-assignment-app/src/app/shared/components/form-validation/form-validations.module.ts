824094088fda571f638827400b1f6aaba2218ae3
Uros Totovic <uto@enigmatry.com>
2024-11-06 22:08:35 +0100

    validations structure

--- enigmatry-angular-assignment-app/src/app/shared/components/form-validation/form-validations.module.ts
+++ enigmatry-angular-assignment-app/src/app/shared/components/form-validation/form-validations.module.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MatError } from '@angular/material/form-field';
-import { FormValidationErrorsComponent } from './form-validation-errors.component';
+import { FormValidationErrorsComponent } from './form-validation-errors/form-validation-errors.component';
 
 @NgModule({
   declarations: [
